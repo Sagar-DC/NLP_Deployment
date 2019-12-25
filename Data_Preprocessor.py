caf962e7f502014a6d8a2138460c6654b279adba
Sagar-DC <Sagar-DC@users.noreply.github.com>
2019-12-26 01:05:07 +0530

    Initial commit

---
+++ Data_Preprocessor.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 25 20:40:15 2019
+
+@author: sagar
+"""
+
+def text_clean(msg):
+    import re
+    from nltk.corpus import stopwords
+    from nltk.stem.porter import PorterStemmer
+
+    stemmer = PorterStemmer()
+    
+    for i in range(0, len([msg])):
+        text = re.sub('[^a-zA-Z]', ' ', msg)
+        text = text.lower()
+        text = text.split()
+    
+        text = [stemmer.stem(word) for word in text if not word in stopwords.words('english')]
+        text = ' '.join(text)
+    
+    return text
+
+        
+
+
---
+++ app.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 25 23:22:49 2019
+
+@author: sagar
+"""
+from flask import Flask,render_template,url_for,request
+import pandas as pd 
+import pickle
+from Data_Preprocessor import text_clean
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+cv = pickle.load(open('tranform.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    msg = request.form['message']
+    text = text_clean(msg)
+    
+    text = [text]
+    vector = cv.transform(text).toarray()
+    prediction = model.predict(vector)
+    
+    if prediction == 0:
+        output = "Ham"
+    else:
+        output = "Spam"
+    
+    return render_template('index.html', prediction_text='Message Entered is "{}" '.format(output))
+
+@app.route('/test', methods=['POST'])
+def test():
+    msg = request.form['message']
+    text = text_clean(msg)
+    return render_template('index.html', prediction_text='Cleaned Message "{}" '.format(text))
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
