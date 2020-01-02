ff0b0deeed0380f3c690ae895cfc2db10f5b6ed3
Sagar-DC <Sagar-DC@users.noreply.github.com>
2020-01-02 14:38:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,34 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Dec 28 18:34:55 2019
-
-@author: sagar
-"""
-
 from flask import Flask,render_template,url_for,request
 import pandas as pd 
 import pickle
 from Data_Preprocessor import text_clean
 
+# load the model from disk
+filename = 'model.pkl'
+clf = pickle.load(open(filename, 'rb'))
+cv=pickle.load(open('tranform.pkl','rb'))  
+cl=pickle.load(open('clean.pkl','rb')) 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
-cv = pickle.load(open('tranform.pkl','rb'))
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+	return render_template('index.html')
 
-@app.route('/predict', methods = ['POST'])
+@app.route('/predict',methods=['POST'])
 def predict():
-    if request.method == 'POST':
-        message = request.form['message']
-        text = text_clean(message)
-        text = [text]
-        vector = cv.transform(text).toarray()
-        prediction = model.predict(vector)
-    return render_template('index.html', prediction_text = prediction)
+	if request.method == 'POST':
+		message = request.form['message']
+		data = [message]
+		vect = cv.transform(data).toarray()
+		my_prediction = clf.predict(vect)
+	return render_template('index.html',prediction_text = my_prediction)
 
-@app.route('/clean', methods = ['POST'])
+@app.route('/clean',methods=['POST'])
 def clean():
-    message = request.form['message']
-    text = text_clean(message)
-    return render_template('index.html', cleaned_text = "Cleaned Message: {}".format(text))
+	if request.method == 'POST':
+		message = request.form['message']
+		text = cl(message)
+	return render_template('index.html',cleaned_text = "Cleaned Message: {}".format(text), actual_text = "Message Entered: {}".format(message)) 
 
-if __name__ == "__main__":
-    app.run(debug = True)
+if __name__ == '__main__':
+	app.run(debug=True)
