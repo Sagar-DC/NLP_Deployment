886bb61cbba44b0e167adac4c0332fd52859b7fd
Sagar-DC <Sagar-DC@users.noreply.github.com>
2020-01-05 18:57:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,17 +32,17 @@ def predict():
 def clean():
     if request.method == 'POST':
         message = request.form['message']
-
-        text = re.sub('[^a-zA-Z]', ' ', message)
-        text = text.lower()
-        text = text.split()
+        
+    text = re.sub('[^a-zA-Z]', ' ', message)
+    text = text.lower()
+    text = text.split()
         #for word in text:
             #if not word in stopwords.words('english'):
                 #tt.append(word)
 
 
         #text = [stemmer.stem(word) for word in text if not word in stopwords.words('english')]
-        text = ' '.join(text)
+    text = ' '.join(text)
             
     return render_template('index.html',cleaned_text = "Stemmed message : {}".format(text), actual_text = "Actual message : {}".format(message))
 
