cabef0e180ec83c17ee5a34639e99ae2a5a0d911
Sagar-DC <Sagar-DC@users.noreply.github.com>
2020-01-15 01:39:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from flask import Flask,render_template,url_for,request
-import pandas as pd 
 import pickle
 
-def text_clean(msg):
-    import re
-    from nltk.corpus import stopwords
-    import nltk
-    nltk.download('stopwords')
-    from nltk.stem.porter import PorterStemmer
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
 
+def stemm_text(msg):
+    from nltk.stem.porter import PorterStemmer
     stemmer = PorterStemmer()
     
     for i in range(0, len([msg])):
@@ -18,14 +17,28 @@ def text_clean(msg):
         
         text = [stemmer.stem(word) for word in text if not word in stopwords.words('english')]
         text = ' '.join(text)
+        
     return text
 
+def lemmatize_text(msg):
+    from nltk.stem import WordNetLemmatizer
+    lemmatizer = WordNetLemmatizer()
+    
+    for i in range(0, len([msg])):
+        text = re.sub('[^a-zA-Z]', ' ', msg)
+        text = text.lower()
+        text = text.split()
+        
+        text = [lemmatizer.lemmatize(word) for word in text if not word in stopwords.words('english')]
+        text = ' '.join(text)
+        
+    return text
+    
 
 # load the model from disk
 filename = 'model.pkl'
 clf = pickle.load(open(filename, 'rb'))
 cv=pickle.load(open('tranform.pkl','rb'))  
-cl=pickle.load(open('clean.pkl','rb')) 
 app = Flask(__name__)
 
 @app.route('/')
@@ -39,22 +52,31 @@ def predict():
     else:
         message = request.args.get('message')
     
-    message = text_clean(message)
+    message = stemm_text(message)
     data = [message]
     vect = cv.transform(data).toarray()
     my_prediction = clf.predict(vect)
-    return render_template('index.html', prediction_text = my_prediction)
+    return render_template('result.html', prediction_text = my_prediction)
 
 
-@app.route('/clean',methods=['POST', 'GET'])
-def clean():
+@app.route('/stemmer',methods=['POST', 'GET'])
+def stemmer():
     if request.method == 'POST':
         message = request.form['message']
     else:
         message = request.args.get('message')
-    text = text_clean(message)
-    #text = message
-    return render_template('index.html',cleaned_text = "Cleaned Message: {}".format(text), actual_text = "Message Entered: {}".format(message)) 
+    text = stemm_text(message)
+    return render_template('result.html',cleaned_text = """Stemmed Message: "{}" """.format(text), actual_text = "Message Entered: {}".format(message)) 
+
+@app.route('/lemmatizer',methods=['POST', 'GET'])
+def lemmatizer():
+    if request.method == 'POST':
+        message = request.form['message']
+    else:
+        message = request.args.get('message')
+    text = lemmatize_text(message)
+    return render_template('result.html',cleaned_text = """Lemmatized Message: "{}" """.format(text), actual_text = "Message Entered: {}".format(message)) 
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
