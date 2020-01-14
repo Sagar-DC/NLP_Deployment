4e6b1ba56a7d074166f5fc9812696761a0d05869
Sagar-DC <Sagar-DC@users.noreply.github.com>
2020-01-14 22:34:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,53 +1,51 @@
 from flask import Flask,render_template,url_for,request
 import pandas as pd 
 import pickle
-import re
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
 
+def text_clean(msg):
+    import re
+    from nltk.corpus import stopwords
+    from nltk.stem.porter import PorterStemmer
+
+    stemmer = PorterStemmer()
+    
+    for i in range(0, len([msg])):
+        text = re.sub('[^a-zA-Z]', ' ', msg)
+        text = text.lower()
+        text = text.split()
+    
+        text = [stemmer.stem(word) for word in text if not word in stopwords.words('english')]
+        text = ' '.join(text)
+    
+    return text
 
 # load the model from disk
 filename = 'model.pkl'
 clf = pickle.load(open(filename, 'rb'))
-cv=pickle.load(open('tranform.pkl','rb')) 
-
-stemmer = PorterStemmer()
-stops = stopwords.words('english')
+cv=pickle.load(open('tranform.pkl','rb'))  
+cl=pickle.load(open('clean.pkl','rb')) 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('home.html')
+	return render_template('index.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    if request.method == 'POST':
-        message = request.form['message']
-        #message = text_clean(message)
-        data = [message]
-        vect = cv.transform(data).toarray()
-        my_prediction = clf.predict(vect)
-    return render_template('index.html',prediction_text = my_prediction, cleaned_text = vect.shape)
+	if request.method == 'POST':
+		message = text_clean(request.form['message'])
+		data = [message]
+		vect = cv.transform(data).toarray()
+		my_prediction = clf.predict(vect)
+	return render_template('index.html',prediction_text = my_prediction)
 
-@app.route('/clean',methods=['POST','GET'])
+
+@app.route('/clean',methods=['POST'])
 def clean():
-    if request.method == 'POST':
-        message = request.form['message']
-        
-# =============================================================================
-#         text = re.sub('[^a-zA-Z]', ' ', message)
-#         text = text.lower()
-#         text = text.split()
-#         #for word in text:
-#             #if not word in stopwords.words('english'):
-#                 #tt.append(word)
-# =============================================================================
-        #text = [stemmer.stem(word) for word in text if not word in stopwords.words('english')]
-        #text = ' '.join(text)
-        text = "hiiiiii"
-            
-    return render_template('index.html',cleaned_text = "Stemmed message : {}".format(text), actual_text = "Actual message : {}".format(message))
-
+	if request.method == 'POST':
+		message = request.form['message']
+		text = text_clean(message)
+	return render_template('index.html',cleaned_text = "Cleaned Message: {}".format(text), actual_text = "Message Entered: {}".format(message)) 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
