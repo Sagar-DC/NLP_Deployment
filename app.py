4d829b94a24a1fdf26fcb97bcda2bd51f47a73ff
Sagar-DC <Sagar-DC@users.noreply.github.com>
2020-01-14 22:53:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def clean():
         message = request.form['message']
     else:
         message = request.args.get('message')
-        text = text_clean(message)
+    text = text_clean(message)
     return render_template('index.html',cleaned_text = "Cleaned Message: {}".format(text), actual_text = "Message Entered: {}".format(message)) 
 
 if __name__ == '__main__':
